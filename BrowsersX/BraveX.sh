6cbf11fdb9768a126c3af8e20b099d0870a42e56
ELCI-Linux <88078492+ELCI-Linux@users.noreply.github.com>
2021-11-03 15:12:30 +0000

    Delete BrowsersX directory

--- BrowsersX/BraveX.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash/
-
-
-echo "You are installing Brave Browser"
-bash BraveXinstall-raw.sh && cat ./elcirecs/BrowsersX/ BraveTOS.txt
--- BrowsersX/Vivaldi.sh
+++
@@ -1,4 +0,0 @@
-#! bin/bash
-
-curl -o VivaldiS64Debian.deb https://downloads.vivaldi.com/stable/vivaldi-stable_4.1.2369.15-1_amd64.deb
-dpkg -i VivaldiS64Debian.deb
