6112ec6dcecdc13bcf2bdafe7571967ac178e80d
ELCI-Linux <88078492+ELCI-Linux@users.noreply.github.com>
2021-11-03 15:24:59 +0000

    Add files via upload

---
+++ installerguide.sh
@@ -0,0 +1,29 @@
+#! /bin/bash/
+
+
+echo -e  "\e[7;32m Welcome to ELCI Installer Guide \e[0m"
+
+echo -e  "\e[0;33m The elci installer is easy to use \e[0m"
+
+echo -e  "\e[0;33m Software has been curated into categories \e[0m"
+
+echo -e  "\e[0;33m As you progress through the installer you will be presented with headers that look like this... \e[0m"
+
+echo -e  "\e[7;33m =========Firewall========= \e[0m"  "\e[5;34m The top line shows what type of software can be installed using each installer \e[0m"
+echo -e  "\e[7;33m ---------Security--------- \e[0m" "\e[5;34m ... whilst the bottom line show the directory location for the installer files \e[0m"
+
+sleep 4s
+
+echo -e  "\e[0;36m Bellow each header will be an description of the type of software that can be installed  \e[0m"
+echo -e  "\e[0;33m After each header and description users will be prompted to select either: \e[0m"
+echo -e  "\e[0;37m Yes - to run the installer(s) \e[0m"  "\e[5;32m Press 1 and then Enter to select this option \e[0m"
+echo -e  "\e[0;37m No - to skip to the next installer \e[0m" "\e[5;31m Press 2 and the Enter to select this option \e[0m"
+
+exit
+
+
+
+
+
+
+
